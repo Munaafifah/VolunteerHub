d30132607e1e1be397cc170d4d8a8172a5da0822
Munaafifah <149042750+Munaafifah@users.noreply.github.com>
2026-08-10 14:36:02 +0800

    Application Naming

--- backend/src/main/java/com/volunteerhub/backend/VolunteerHubApplication.java
+++ backend/src/main/java/com/volunteerhub/backend/VolunteerHubApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class VolunteerHubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(VolunteerHubApplication.class, args);
 	}
 
 }
